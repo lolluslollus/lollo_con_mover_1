0717bffe17c86bad76a10f7952cfcc2c8fcd18df
lolluslollus <lolluslollus@hotmail.com>
2022-02-23 00:03:23 +0100

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -3,13 +3,13 @@ function data()
 		en = {
             ["ModDesc"] =
                 [[
-Turn this on and off from the bottom bar.
+                    Turn this on and off from the bottom bar.
 When it is on and you select certain constructions, a popup will appear.
 Here, you can shift and rotate the construction after it has been built.
 
 NOTES:
 - Not every construction can be rearranged.
-- Constructions with bits of road or rail may require readjusting of those roads and rails.
+- Constructions with bits of road or rail may require readjusting of those roads and rails. The splits could be invisible and annoying to handle. As a rule of thumb, constructions that lock edges should give the option of snapping them to their neighbours (like depots), and you should use it. The street fine tuning (merges, tight bends, chunks) and the freestyle station (stairs, lifts) can handle this well, unless you purposedly unsnap a construction after building it.
 			]],
             ["ModName"] = "Construction Mover",
             ["ConId"] = "Con Id = ",
