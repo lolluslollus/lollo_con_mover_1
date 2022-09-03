b970d1d99094c35f0726526b657ee6f82979e020
lolluslollus <lolluslollus@users.noreply.github.com>
2022-09-04 01:17:35 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -8,7 +8,7 @@ When it is on and you select certain constructions, a popup will appear.
 Here, you can shift and rotate the construction after it has been built.
 
 NOTES:
-- Not every construction can be rearranged.
+- Only certain types of constructions can be rearranged, mostly assets. Stations, depots or town buildings cannot.
 - Constructions with bits of road may require readjusting those roads. The splits could be invisible and annoying to handle. To make life easier, those constructions should give the option of snapping roads to their neighbours (like depots), and you should use it. The street fine tuning (merges, tight bends, chunks) and the freestyle station (stairs, lifts) can handle this well.
 			]],
             ["ModName"] = "Construction Mover",
