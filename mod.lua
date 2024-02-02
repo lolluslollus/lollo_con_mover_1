8cb10a25b7a9edb233327e7a94886a771af5d495
lolluslollus <lolluslollus@users.noreply.github.com>
2024-02-02 20:28:22 +0100

    filter the cons to be made moveable

--- mod.lua
+++ mod.lua
@@ -1,9 +1,36 @@
 local logger = require('lolloConMover.logger')
+local stringUtils = require('lolloConMover.stringUtils')
 local utils = require('lolloConMover.utils')
 
 function data()
+	local _allowedConTypes = {
+		'ASSET_DEFAULT',
+		'ASSET_TRACK',
+		'STREET_CONSTRUCTION',
+		'TRACK_CONSTRUCTION',
+	}
+	local _excludedConFileNames = {
+		'wk_arrow_end.con',
+		'wk_arrow_start.con',
+		'wk_info.con',
+		'wk_marker_01.con',
+		'wk_marker_02.con',
+		'wk_mrkx_delete_edges.con',
+		'wk_mrkx_make_parallel.con',
+		'wk_mrkx_modify.con',
+	}
 	local function loadConstructionFunc(fileName, data)
 		if not(data) or (type(data.updateFn) ~= 'function') then return data end
+
+		local isConTypeAllowed = false
+		for _, cty in pairs(_allowedConTypes) do
+			if data.type == cty then isConTypeAllowed = true break end
+		end
+		if not(isConTypeAllowed) then return data end
+
+		for _, fna in pairs(_excludedConFileNames) do
+			if stringUtils.stringEndsWith(fileName, fna) then return data end
+		end
 
 		-- LOLLO TODO do we need this? We probably do with some mods. I took it out with minor 13.
 		-- if type(data.upgradeFn) ~= 'function' then
@@ -11,6 +38,10 @@ function data()
 		-- else
 		-- 	logger.print('upgradeFn() found for', fileName)
 		-- end
+		logger.print('loadConstructionFunc starting for con with filename = ' .. fileName .. '; data.type = ' .. tostring(data.type))
+		-- 1962843709 (40 tons Truck Set) has a dynamic updateFn and does not work, neither with runFn() nor with postRunFn()
+		-- if stringUtils.stringEndsWith(fileName, 'nando_truck_set.con') then logger.print('nando con data =') logger.debugPrint(data) end
+		-- if stringUtils.stringEndsWith(fileName, 'nando_truck_set.script') then logger.print('nando script data =') logger.debugPrint(data) end
 
 		local _originalUpdateFn = data.updateFn
 		data.updateFn = function(params)
@@ -42,7 +73,7 @@ function data()
 
 	return {
 		info = {
-			minorVersion = 10,
+			minorVersion = 11,
 			severityAdd = "NONE",
 			severityRemove = "NONE",
 			name = _("ModName"),
